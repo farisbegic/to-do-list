3ff1188bbb64f34aba17f4a3b276b5f7c9a5ed69
farisbegic <faris.begic@stu.ssst.edu.ba>
2022-05-13 11:17:44 +0200

    Added env variables

--- frontend/src/app/todoApi.js
+++ frontend/src/app/todoApi.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const { REACT_APP_BACKEND_URL } = process.env;
+
 export default axios.create({
-    baseURL: "https://localhost:5001"
+    baseURL: REACT_APP_BACKEND_URL ? REACT_APP_BACKEND_URL : "https://localhost:5001"
 })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {store} from './app/store'
 import {Provider} from 'react-redux'
-import ToDo from "./Components/ToDo/ToDo";
+import ToDo from "./components/ToDo/ToDo";
 import './index.css'
 
 ReactDOM.render(
